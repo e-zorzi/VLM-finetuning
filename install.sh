38dcf3bef0e13c9bedef874efaa633a8adb5485e
edoardo <edo_id@icloud.com>
2025-12-11 13:54:30 +0100

    Add last sweep configs (H100)

---
+++ install.sh
@@ -0,0 +1,7 @@
+export UV_TORCH_BACKEND=cu126
+uv venv --no-project --relocatable
+source .venv/bin/activate
+uv pip install packaging setuptools wheel
+uv pip install torch==2.6.0 torchvision
+uv pip install awscli pydantic
+uv pip install --no-build-isolation axolotl[flash-attn]
