d12db976455b619428ae030f651f9c5a9a7a472d
edoardo <edo_id@icloud.com>
2026-01-01 14:54:04 +0100

    new configs

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ source .venv/bin/activate
 uv pip install packaging setuptools wheel
 uv pip install torch==2.6.0 torchvision
 uv pip install awscli pydantic
-uv pip install --no-build-isolation axolotl[flash-attn]
+uv pip install --no-build-isolation axolotl[deepspeed,flash-attn]
+echo "Remember to export your HF_TOKEN and WANDB login token"
